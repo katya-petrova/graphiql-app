288a058804aa3659708731eaf1a2946736003dff
Pavel Gordienko <guzmo@yandex.ru>
2024-08-21 19:20:12 +0300

    feat: add test for Loader

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -3,7 +3,10 @@ import './Loader.css';
 
 const Loader: React.FC = () => {
   return (
-    <div className="flex justify-center items-center min-h-screen">
+    <div
+      className="flex justify-center items-center min-h-screen"
+      data-testid="loader-container"
+    >
       <div className="loader"></div>
     </div>
   );
