4a9b2b9b6b94d1bd20202a2b5368f4f31d62d5a7
Pavel Gordienko <guzmo@yandex.ru>
2024-08-21 20:18:26 +0300

    feat: add test for Form

--- src/components/Signup.tsx
+++ src/components/Signup.tsx
@@ -1,7 +1,7 @@
 'use client';
 import React, { useEffect, useState } from 'react';
 import { Button } from './Button/Button';
-import { Form } from './Form';
+import { Form } from './Form/Form';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { useRouter } from 'next/navigation';
 import Loader from './Loader/Loader';
