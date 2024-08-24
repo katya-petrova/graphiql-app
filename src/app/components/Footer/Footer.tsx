1f15a917559db2c5e5b2bf4ce8dfe64ef487743a
Katsiaryna Piatrova <katsiaryna_piatrova@epam.com>
2024-08-24 14:14:24 +0400

    update footer with dev git hubs links

--- src/app/components/Footer/Footer.tsx
+++ src/app/components/Footer/Footer.tsx
@@ -4,16 +4,38 @@ export const Footer = () => {
   return (
     <footer className="bg-blue-600 text-white py-2 md:py-3">
       <div className="px-2 md:px-3 w-full text-center flex justify-between items-center">
-        <a className="px-5 rounded-md" href="https://github.com/">
-          <Image
-            src="/logos/github-logo.svg"
-            alt="GitHub Logo"
-            className="dark:invert"
-            width={32}
-            height={32}
-          />
-        </a>
+        <div className="developers flex gap-4">
+          <a className="rounded-md" href="https://github.com/katya-petrova/">
+            <Image
+              src="/logos/github-logo.svg"
+              alt="GitHub Logo"
+              className="dark:invert"
+              width={32}
+              height={32}
+            />
+          </a>
+          <a className="rounded-md" href="https://github.com/guz86/">
+            <Image
+              src="/logos/github-logo.svg"
+              alt="GitHub Logo"
+              className="dark:invert"
+              width={32}
+              height={32}
+            />
+          </a>
+          <a className="rounded-md" href="https://github.com/krsisabi/">
+            <Image
+              src="/logos/github-logo.svg"
+              alt="GitHub Logo"
+              className="dark:invert"
+              width={32}
+              height={32}
+            />
+          </a>
+        </div>
+
         <p className="text-sm md:text-base">© 2024 GraphiQL</p>
+
         <a className="px-5 rounded-md" href="https://rs.school/">
           <Image
             src="/logos/rss-logo.svg"
