99bc865b5984da7570def014da12b0d95907723b
Pavel Gordienko <guzmo@yandex.ru>
2024-08-30 03:04:38 +0300

    fix: change button color

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -15,7 +15,7 @@ const Button: React.FC<ButtonProps> = ({
     <button
       type={type}
       onClick={onClick}
-      className="px-4 py-2 border rounded bg-yellow-500 text-white hover:bg-yellow-600"
+      className="px-4 py-2 border rounded bg-blue-500 text-white hover:bg-blue-600"
     >
       {children}
     </button>
