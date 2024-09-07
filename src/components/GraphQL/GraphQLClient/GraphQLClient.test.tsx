83784b1f92e50abddd1b4af3dd4cfb91281ffc48
Pavel Gordienko <guzmo@yandex.ru>
2024-09-07 14:12:28 +0300

    feat: add tests for graphql

--- src/components/GraphQL/GraphQLClient/GraphQLClient.test.tsx
+++ src/components/GraphQL/GraphQLClient/GraphQLClient.test.tsx
@@ -1,147 +1,129 @@
-// import { render, screen, fireEvent, waitFor } from '@testing-library/react';
-// import { describe, it, expect, vi, beforeEach } from 'vitest';
-// import GraphQLClient from './GraphQLClient';
-// import { MockedProvider } from '@apollo/client/testing';
-// import { ToastContainer } from 'react-toastify';
+import { render, screen, fireEvent, waitFor } from '@testing-library/react';
+import { describe, it, expect, vi, beforeEach } from 'vitest';
+import GraphQLClient from './GraphQLClient';
+import { MockedProvider } from '@apollo/client/testing';
 
-// vi.mock('../QueryResult/QueryResult', () => ({
-//   __esModule: true,
-//   default: ({ queryResult, error, statusCode, loading }: any) => (
-//     <div data-testid="query-result">
-//       {loading && <div>Loading...</div>}
-//       {error && <div>Error: {error}</div>}
-//       {statusCode && <div>Status: {statusCode}</div>}
-//       {queryResult && <pre>{JSON.stringify(queryResult)}</pre>}
-//     </div>
-//   ),
-// }));
+vi.mock('@/utils/graphqlClient/useGraphQLClient', () => ({
+  __esModule: true,
+  default: vi.fn(() => ({
+    client: {
+      query: vi
+        .fn()
+        .mockResolvedValue({ data: { country: { name: 'United States' } } }),
+    },
+    setUrl: vi.fn(),
+    setHeaders: vi.fn(),
+  })),
+}));
 
-// vi.mock('../SdlDocumentation/SdlDocumentation', () => ({
-//   __esModule: true,
-//   default: ({ sdlData }: any) => (
-//     <div data-testid="sdl-documentation">
-//       {sdlData ? <pre>{sdlData}</pre> : 'No SDL Data'}
-//     </div>
-//   ),
-// }));
+vi.mock('../QueryResult/QueryResult', () => ({
+  __esModule: true,
+  default: ({ queryResult, error, statusCode, loading }: any) => (
+    <div data-testid="query-result">
+      {loading && <div>Loading...</div>}
+      {error && <div>Error: {error}</div>}
+      {statusCode && <div>Status: {statusCode}</div>}
+      {queryResult && <pre>{JSON.stringify(queryResult)}</pre>}
+    </div>
+  ),
+}));
 
-// vi.mock('../QueryForm/QueryForm', () => ({
-//   __esModule: true,
-//   default: ({
-//     onUrlChange,
-//     onSdlUrlChange,
-//     onQueryChange,
-//     onVariablesChange,
-//     onHeadersChange,
-//     onQueryExecute,
-//   }: any) => (
-//     <div>
-//       <input placeholder="Endpoint URL" onChange={onUrlChange} />
-//       <input placeholder="SDL URL" onChange={onSdlUrlChange} />
-//       <textarea placeholder="Query" onChange={onQueryChange} />
-//       <textarea placeholder="Variables" onChange={onVariablesChange} />
-//       <textarea
-//         placeholder="Headers"
-//         onChange={(e) => onHeadersChange(e.target.value)}
-//       />
-//       <button onClick={onQueryExecute}>Send Request</button>
-//     </div>
-//   ),
-// }));
+vi.mock('../SdlDocumentation/SdlDocumentation', () => ({
+  __esModule: true,
+  default: ({ sdlData }: any) => (
+    <div data-testid="sdl-documentation">
+      {sdlData ? <pre>{sdlData}</pre> : 'No SDL Data'}
+    </div>
+  ),
+}));
 
-// vi.mock('../SdlFetcher/SdlFetcher', () => ({
-//   __esModule: true,
-//   default: ({ sdlUrl, headers, onSdlDataFetch, onError }: any) => (
-//     <div>
-//       <button onClick={() => onSdlDataFetch('Fake SDL Data')}>Fetch SDL</button>
-//     </div>
-//   ),
-// }));
+vi.mock('../QueryForm/QueryForm', () => ({
+  __esModule: true,
+  default: ({
+    onUrlChange,
+    onSdlUrlChange,
+    onQueryChange,
+    onVariablesChange,
+    onHeadersChange,
+    onQueryExecute,
+  }: any) => (
+    <div>
+      <input placeholder="Endpoint URL" onChange={onUrlChange} />
+      <input placeholder="SDL URL" onChange={onSdlUrlChange} />
+      <textarea placeholder="Query" onChange={onQueryChange} />
+      <textarea placeholder="Variables" onChange={onVariablesChange} />
+      <textarea
+        placeholder="Headers"
+        onChange={(e) => onHeadersChange(e.target.value)}
+      />
+      <button onClick={onQueryExecute}>Send Request</button>
+    </div>
+  ),
+}));
 
-// vi.mock('../../ToastContainer/ToastContainer', () => ({
-//   __esModule: true,
-//   default: () => <ToastContainer />,
-// }));
+vi.mock('../SdlFetcher/SdlFetcher', () => ({
+  __esModule: true,
+  default: ({ onSdlDataFetch, onError }: any) => (
+    <div>
+      <button onClick={() => onSdlDataFetch('Fake SDL Data')}>Fetch SDL</button>
+      <button onClick={() => onError('Mock Error')}>Trigger Error</button>
+    </div>
+  ),
+}));
 
-// describe('GraphQLClient Component', () => {
-//   beforeEach(() => {
-//     vi.clearAllMocks();
-//   });
+vi.mock('../../ToastContainer/ToastContainer', () => ({
+  __esModule: true,
+  default: () => <div>ToastContainer Mock</div>,
+}));
 
-//   it('renders the component with all children', () => {
-//     render(
-//       <MockedProvider>
-//         <GraphQLClient />
-//       </MockedProvider>
-//     );
+vi.mock('next/navigation', async (importOriginal) => {
+  const actual: {
+    useSearchParams: () => URLSearchParams;
+    usePathname: () => string;
+  } = await importOriginal();
+  return {
+    ...actual,
+    usePathname: vi.fn().mockReturnValue('/'),
+    useSearchParams: vi.fn().mockReturnValue(new URLSearchParams('')),
+  };
+});
 
-//     expect(screen.getByText('GraphQL Client')).toBeInTheDocument();
-//     expect(screen.getByPlaceholderText('Endpoint URL')).toBeInTheDocument();
-//     expect(screen.getByPlaceholderText('SDL URL')).toBeInTheDocument();
-//     expect(screen.getByPlaceholderText('Query')).toBeInTheDocument();
-//     expect(screen.getByPlaceholderText('Variables')).toBeInTheDocument();
-//     expect(screen.getByPlaceholderText('Headers')).toBeInTheDocument();
-//     expect(screen.getByText('Send Request')).toBeInTheDocument();
-//     expect(screen.getByText('Fetch SDL')).toBeInTheDocument();
-//   });
+describe('GraphQLClient Component', () => {
+  beforeEach(() => {
+    vi.clearAllMocks();
+  });
 
-//   it('handles input changes and fetches data', async () => {
-//     render(
-//       <MockedProvider>
-//         <GraphQLClient />
-//       </MockedProvider>
-//     );
+  it('renders the component with all children', () => {
+    render(
+      <MockedProvider>
+        <GraphQLClient />
+      </MockedProvider>
+    );
 
-//     fireEvent.change(screen.getByPlaceholderText('Endpoint URL'), {
-//       target: { value: 'https://new-url.com' },
-//     });
-//     fireEvent.change(screen.getByPlaceholderText('SDL URL'), {
-//       target: { value: 'https://new-sdl-url.com' },
-//     });
-//     fireEvent.change(screen.getByPlaceholderText('Query'), {
-//       target: { value: 'query Test { test }' },
-//     });
-//     fireEvent.change(screen.getByPlaceholderText('Variables'), {
-//       target: { value: '{"key": "value"}' },
-//     });
-//     fireEvent.change(screen.getByPlaceholderText('Headers'), {
-//       target: { value: '[{"key": "Authorization", "value": "Bearer token"}]' },
-//     });
+    expect(screen.getByText('GraphQL Client')).toBeInTheDocument();
+    expect(screen.getByPlaceholderText('Endpoint URL')).toBeInTheDocument();
+    expect(screen.getByPlaceholderText('SDL URL')).toBeInTheDocument();
+    expect(screen.getByPlaceholderText('Query')).toBeInTheDocument();
+    expect(screen.getByPlaceholderText('Variables')).toBeInTheDocument();
+    expect(screen.getByPlaceholderText('Headers')).toBeInTheDocument();
+    expect(screen.getByText('Send Request')).toBeInTheDocument();
+    expect(screen.getByText('Fetch SDL')).toBeInTheDocument();
+    expect(screen.getByText('ToastContainer Mock')).toBeInTheDocument();
+  });
 
-//     fireEvent.click(screen.getByText('Send Request'));
+  it('fetches SDL data and displays it', async () => {
+    render(
+      <MockedProvider>
+        <GraphQLClient />
+      </MockedProvider>
+    );
 
-//     await waitFor(() => {
-//       expect(screen.getByText('Loading...')).toBeInTheDocument();
-//     });
-//   });
+    fireEvent.click(screen.getByText('Fetch SDL'));
 
-//   it('fetches SDL data and displays it', async () => {
-//     render(
-//       <MockedProvider>
-//         <GraphQLClient />
-//       </MockedProvider>
-//     );
-
-//     fireEvent.click(screen.getByText('Fetch SDL'));
-
-//     await waitFor(() => {
-//       expect(screen.getByTestId('sdl-documentation')).toHaveTextContent(
-//         'Fake SDL Data'
-//       );
-//     });
-//   });
-
-//   it('displays errors correctly', async () => {
-//     render(
-//       <MockedProvider>
-//         <GraphQLClient />
-//       </MockedProvider>
-//     );
-
-//     const queryError = 'Error executing the query!';
-//     vi.spyOn(console, 'error').mockImplementation(() => {});
-//     await waitFor(() => {
-//       expect(screen.getByText(queryError)).toBeInTheDocument();
-//     });
-//   });
-// });
+    await waitFor(() => {
+      expect(screen.getByTestId('sdl-documentation')).toHaveTextContent(
+        'Fake SDL Data'
+      );
+    });
+  });
+});
