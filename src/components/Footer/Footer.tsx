fb09871e57808581f4a906cd119ef56db0af4b32
Tim Yusupov <timboyus@gmail.com>
2024-09-09 11:07:12 +0300

    add footer styles

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,11 +3,11 @@ import { Link } from '../Link';
 
 export const Footer = () => {
   return (
-    <footer className="bg-blue-600 text-white md:px-5 py-2 md:py-3">
-      <div className="px-2 md:px-3 w-full text-center flex justify-between items-center">
-        <div className="developers flex gap-4">
+    <footer className="bg-blue-600 text-white px-2 md:px-5 py-2 md:py-3">
+      <div className="flex justify-between sm:grid sm:grid-cols-3 px-2 md:px-3 w-full text-center items-center">
+        <div className="developers flex gap-2 md:gap-4">
           <Link
-            className="rounded-md"
+            className="rounded-full hover:opacity-80 transition-all"
             href="https://github.com/katya-petrova/"
             external
           >
@@ -20,7 +20,7 @@ export const Footer = () => {
             />
           </Link>
           <Link
-            className="rounded-md"
+            className="rounded-full hover:opacity-80 transition-all"
             href="https://github.com/guz86/"
             external
           >
@@ -33,7 +33,7 @@ export const Footer = () => {
             />
           </Link>
           <Link
-            className="rounded-md"
+            className="rounded-full hover:opacity-80 transition-all"
             href="https://github.com/krsisabi/"
             external
           >
@@ -50,6 +50,7 @@ export const Footer = () => {
         <p className="text-sm md:text-base">© 2024 GraphiQL</p>
 
         <Link
+          className="rounded-full justify-self-end hover:opacity-80 transition-all"
           href="https://rs.school/"
           target="_blank"
           rel="noopener noreferrer"
